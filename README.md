302ca3d2bbab8286b1d9b0b22b88212cda492bd3
tritranduc <tritranduc11@gmail.com>
2021-12-08 09:03:00 +0700

    add session cookie
    - fix some bug for server

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # new-server-express-have-typescript-graphql
+
+xin chào các bạn nếu các bạn đã xem video demo về cái server này của tớ thì tớ xin lỗi vì tớ đã thay đổi một số thứ trong code nhưng không sao cái này nó ko ảnh hưởng đến việc chạy server nên các bạn không cần phải lo
+hiện tại tớ mới thêm session cookie
+và tớ sẽ viết  lại model bằng typegoose trong thời gian nhanh nhất
--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,43 @@ import dotenv from "dotenv";
 import connectToDB from "./util/connectToDB";
 import { buildSchema } from "type-graphql";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import { HelloResolver } from "./resolvers/hello";
+import { COOKIE_NAME } from "./constraint";
+import { Context } from "./types/Context";
 dotenv.config({ path: path.resolve(__dirname, "./.env") });
 const main = async () => {
   const app = express();
-  await connectToDB(process.env.DB_URL as string);
+  const MongoUrl = process.env.DB_URL as string;
+
+  await connectToDB(MongoUrl);
+  app.use(
+    session({
+      name: COOKIE_NAME,
+      store: MongoStore.create({ mongoUrl: MongoUrl }),
+      cookie: {
+        maxAge: 1000 * 60 * 60, // 1 HOURS
+        httpOnly: true,
+        sameSite: "lax", //csrf
+        secure: process.env.NODE_ENV === "production", //cookie only works in https
+      },
+      secret: process.env.COOKIE_SECRET as string,
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver],
       validate: false,
     }),
     plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+    context: ({ req, res }): Context => {
+      return { req, res };
+    },
   });
+  //use the session middleware req.session.userId = userId
   await apolloServer.start();
   apolloServer.applyMiddleware({ app, cors: false });
   const HttpServer = createServer(app);
